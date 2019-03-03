de0dca0878bc5f364a645e79055fd1dd09e714a1
Denis <denis.da.by@gmail.com>
2019-03-03 20:57:14 +0300

    Initial commit

---
+++ src/BaseDBCache.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace DBCache\email;
+
+abstract class BaseDBCache
+{
+	private $currentData;
+	private $oldData;
+	private $primaryColumn;
+
+
+	public function __construct()
+	{
+		$this->currentData = $this->oldData = $this->getDataFromDB();
+		$this->primaryColumn = $this->initPrimaryColumn();
+	}
+
+	protected abstract function getDataFromDB(): array;
+
+	protected abstract function initPrimaryColumn(): string;
+
+	public function add($item): void
+	{
+		if ($this->validate($item) and !$this->check($item[]))
+			$this->currentData[$item[$this->primaryColumn]] = $item;
+	}
+
+	protected abstract function validate($item): bool;
+
+	function check($item): bool
+	{
+		if (isset($this->curentData[$item[$this->primaryColumn]]))
+			return true;
+		else
+			return false;
+	}
+
+	public function delete($item): void
+	{
+		unset($this->currentData[$item[$this->primaryColumn]]);
+	}
+
+	public function saveDataToDB()
+	{
+		$deletedItems = $this->getDeletedItems();
+		$newItems = $this->getNewItems();
+		$updatedItems = $this->getUpdatedItems();
+	}
+
+	private function getDeletedItems(): array
+	{
+		return array_diff_key($this->oldData, $this->currentData);
+	}
+
+	private function getNewItems(): array
+	{
+		return array_diff_key($this->currentData, $this->oldData);
+	}
+
+	private function getUpdatedItems(): array
+	{
+		$updatedItems = [];
+		foreach (array_intersect_key($this->currentData, $this->oldData) as $item)
+			if ($this->currentData[$item[$this->primaryColumn]] != $this->oldData[$item[$this->primaryColumn]])
+				$updatedItems[] = $item;
+		return $updatedItems;
+	}
+
+	protected abstract function deleteItemsFromDb($data): void;
+
+	protected abstract function updateItemsFromDb($data): void;
+
+	protected abstract function insertItemsInDb($data): void;
+
+
+}
---
+++ src/Email.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DBCache\email;
+
+class email extends BaseDBCache
+{
+
+
+	protected function getDataFromDB(): array
+	{
+		return DB::select('select * from emails');
+	}
+
+
+	protected function validate($item): bool
+	{
+		if (filter_var($item, FILTER_VALIDATE_EMAIL))
+			return true;
+		else
+			return false;
+	}
+
+	protected function initPrimaryColumn(): string
+	{
+		return 'address';
+	}
+
+	protected function deleteItemsFromDb($data): void
+	{
+		$ids_to_delete = array_column($data, 'id');
+		DB::table('email')->whereIn('id', $ids_to_delete)->delete();
+	}
+
+	protected function updateItemsFromDb($data): void
+	{
+//		foreach ($data as $item) {
+//			DB::table('email')->where('address', $item['address'])->update($item);
+//		}
+	}
+
+	protected function insertItemsInDb($data): void
+	{
+		DB::table('email')->insert($data);
+	}
+}
